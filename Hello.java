1e76e2a7921b3688619a1a116866b80ba6a63548
Jeevan R <jeevan.rajkumar2023@vitstudent.ac.in>
2026-02-14 22:02:31 +0530

    Fix syntax error in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Mini CI Project Successful after few minutes");
+        System.out.println("Mini CI Project Successful after few minutes")
     }
 }
